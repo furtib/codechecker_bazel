2f8f18ae93d7637e4ec3e8bf8dcb47fecd585ff4
Kristóf Umann <dkszelethus@gmail.com>
2025-06-17 14:39:23 +0200

    Make line lengths <79 in test.py
    I also made sure to skip one of the tests if clang-extdef-mapping is
    missing, though maybe it should be promoted to a hard error in the rule
    definition.

--- test/test.py
+++ test/test.py
@@ -19,6 +19,7 @@ import logging
 import os
 import re
 import shlex
+import shutil
 import subprocess
 import sys
 import unittest
@@ -70,14 +71,13 @@ class TestBase(unittest.TestCase):
         """Grep given filename"""
         pattern = re.compile(regex)
         logging.debug("RegEx = r'%s'", regex)
-        with open(filename, "r") as fileobj:
+        with open(filename, "r", encoding="utf-8") as fileobj:
             for line in fileobj:
                 if pattern.search(line):
                     logging.debug(line)
                     return line
         self.fail(f"Could not find r'{regex}' in '{filename}'")
         return ""
-
 
 class TestBasic(TestBase):
     """Basic tests"""
@@ -100,7 +100,9 @@ class TestBasic(TestBase):
         self.check_command("bazel test :codechecker_fail", exit_code=3)
         logfile = os.path.join(
             self.BAZEL_BIN_DIR, "codechecker_fail", "codechecker.log")
-        self.grep_file(logfile, r"clang-diagnostic-unused-variable\s+\|\s+MEDIUM\s+\|\s+1")
+        self.grep_file(
+            logfile,
+            r"clang-diagnostic-unused-variable\s+\|\s+MEDIUM\s+\|\s+1")
         self.grep_file(logfile, r"core.NullDereference\s+\|\s+HIGH\s+\|\s+1")
         self.grep_file(logfile, r"deadcode.DeadStores\s+\|\s+LOW\s+\|\s+1")
         self.grep_file(logfile, r"lib.cc\s+\|\s+3")
@@ -118,25 +120,28 @@ class TestBasic(TestBase):
 
     def test_bazel_compile_commands(self):
         """Test: bazel build --build_tag_filters=compile_commands ..."""
-        self.check_command("bazel build --build_tag_filters=compile_commands ...")
+        self.check_command(
+            "bazel build --build_tag_filters=compile_commands ...")
         compile_commands = os.path.join(
-            self.BAZEL_BIN_DIR, "compile_commands_pass", "compile_commands.json")
+            self.BAZEL_BIN_DIR, "compile_commands_pass",
+            "compile_commands.json")
         self.grep_file(compile_commands, r"pass\.cc")
         self.grep_file(compile_commands, r"bin\/gcc")
 
     def test_bazel_aspect_clang_tidy_pass(self):
         """Test: bazel build :test_pass --aspects"""
-        command = "bazel build :test_pass"
-        command += " --aspects @bazel_codechecker//src:clang.bzl%clang_tidy_aspect"
-        command += " --output_groups=report"
+        command = "bazel build :test_pass " + \
+            "--aspects @bazel_codechecker//src:clang.bzl%clang_tidy_aspect" + \
+            " --output_groups=report"
         self.check_command(command, exit_code=0)
 
     def test_bazel_aspect_clang_tidy_fail(self):
         """Test: bazel build :test_lib --aspects"""
-        # NOTE: we should use :test_fail but transitive dependencies do not work
-        command = "bazel build :test_lib"
-        command += " --aspects @bazel_codechecker//src:clang.bzl%clang_tidy_aspect"
-        command += " --output_groups=report"
+        # NOTE: we should use :test_fail but transitive dependencies do not
+        # work
+        command = "bazel build :test_lib " + \
+            "--aspects @bazel_codechecker//src:clang.bzl%clang_tidy_aspect" + \
+            " --output_groups=report"
         self.check_command(command, exit_code=1)
 
     def test_bazel_build_clang_tidy_pass(self):
@@ -157,12 +162,15 @@ class TestBasic(TestBase):
 
     def test_bazel_build_clang_ctu(self):
         """Test: bazel build :clang_ctu_pass :clang_ctu_fail"""
-        self.check_command("bazel build :clang_ctu_pass :clang_ctu_fail", exit_code=0)
+        self.check_command(
+            "bazel build :clang_ctu_pass :clang_ctu_fail", exit_code=0)
 
     def test_bazel_test_clang_ctu_pass(self):
         """Test: bazel test :clang_ctu_pass"""
         self.check_command("bazel test :clang_ctu_pass", exit_code=0)
 
+    @unittest.skipIf(shutil.which("clang-extdef-mapping") is None,
+                     "Missing 'clang-extdef-mapping")
     def test_bazel_test_clang_ctu_fail(self):
         """Test: bazel test :clang_ctu_fail"""
         self.check_command("bazel test :clang_ctu_fail", exit_code=3)
@@ -179,7 +187,9 @@ class TestBasic(TestBase):
         self.check_command("bazel test :code_checker_fail", exit_code=3)
         logfile = os.path.join(
             self.BAZEL_TESTLOGS_DIR, "code_checker_fail", "test.log")
-        self.grep_file(logfile, r"clang-diagnostic-unused-variable\s+\|\s+MEDIUM\s+\|\s+1")
+        self.grep_file(
+            logfile,
+            r"clang-diagnostic-unused-variable\s+\|\s+MEDIUM\s+\|\s+1")
         self.grep_file(logfile, r"core.NullDereference\s+\|\s+HIGH\s+\|\s+1")
         self.grep_file(logfile, r"deadcode.DeadStores\s+\|\s+LOW\s+\|\s+1")
         self.grep_file(logfile, r"lib.cc\s+\|\s+3")
