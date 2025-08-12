c43ef778e085fd6cfc63ee119fc35a6bf8c40817
F.Tibor <tibi.furesz@gmail.com>
2025-08-12 13:37:10 +0200

    Only run test if compilation is successful

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --network host -t my-test-env .
-docker run -it --rm -v $(pwd):/app --network host my-test-env
+docker build --network host -t my-test-env . && \
+docker run -it --rm -v $(pwd):/app -v $(pwd)/tmp:/app/* --network host my-test-env
