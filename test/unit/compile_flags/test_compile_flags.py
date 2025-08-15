426e46295de0819ff68de0e47933460e80ea9be1
F.Tibor <tibi.furesz@gmail.com>
2025-08-15 11:23:03 +0200

    Use common library

--- test/unit/compile_flags/test_compile_flags.py
+++ test/unit/compile_flags/test_compile_flags.py
@@ -22,61 +22,8 @@ import shlex
 import subprocess
 import sys
 import unittest
+from ..common.base import TestBase
 
-
-class TestBase(unittest.TestCase):
-    """Unittest base abstract class"""
-
-    BAZEL_BIN_DIR = os.path.join("../../..", "bazel-bin", "test", "unit", "compile_flags")
-    BAZEL_TESTLOGS_DIR = os.path.join("../../..", "bazel-testlogs", "test", "unit", "compile_flags")
-
-    @classmethod
-    def setUpClass(cls):
-        """Load module, save environment"""
-        # Save environment and location
-        cls.save_env = os.environ
-        cls.save_cwd = os.getcwd()
-        # Move to test dir
-        cls.test_dir = os.path.abspath(os.path.dirname(__file__))
-        os.chdir(cls.test_dir)
-
-    @classmethod
-    def tearDownClass(cls):
-        """Restore environment"""
-        os.chdir(cls.save_cwd)
-        os.environ = cls.save_env
-
-    def setUp(self):
-        """Before every test"""
-        logging.debug("\n%s", "-" * 70)
-
-    def check_command(self, cmd, exit_code=0):
-        """Run shell command and check status"""
-        logging.debug("Running: %s", cmd)
-        commands = shlex.split(cmd)
-        with subprocess.Popen(commands,
-                              stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE) as process:
-            stdout, stderr = process.communicate()
-            self.assertEqual(
-                process.returncode,
-                exit_code, "\n" + "\n".join([
-                    f"command: {cmd}",
-                    f"stdout: {stdout.decode('utf-8')}",
-                    f"stderr: {stderr.decode('utf-8')}"]))
-
-    def grep_file(self, filename, regex):
-        """Grep given filename"""
-        pattern = re.compile(regex)
-        logging.debug("RegEx = r'%s'", regex)
-        with open(filename, "r", encoding="utf-8") as fileobj:
-            for line in fileobj:
-                if pattern.search(line):
-                    logging.debug(line)
-                    return True
-        return False
-        self.fail(f"Could not find r'{regex}' in '{filename}'")
 
 class TestBasic(TestBase):
     """Basic tests"""
