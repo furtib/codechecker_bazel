e6e0dd05f280884317795eb7556453a1d1f6cec4
F.Tibor <tibi.furesz@gmail.com>
2025-09-03 14:47:55 +0200

    Add Local foss test runner

---
+++ test/foss/test_foss.py
@@ -0,0 +1,94 @@
+import logging
+import shlex
+import subprocess
+import sys
+import unittest
+import os
+
+ROOT_DIR = "./"
+
+NOT_PROJECT_FOLDERS = ["templates", "__pycache__", ".pytest_cache"]
+
+
+def get_dynamic_test_dirs():
+    dirs = []
+    for entry in os.listdir(ROOT_DIR):
+        full_path = os.path.join(ROOT_DIR, entry)
+        if os.path.isdir(full_path) and entry not in NOT_PROJECT_FOLDERS:
+            dirs.append(entry)
+    return dirs
+
+
+PROJECT_DIRS = get_dynamic_test_dirs()
+
+
+# This will contain the generated tests.
+class DynamicFOSSTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Configure logging before running tests"""
+        # Enable debug logs for tests if "super verbose" flag is provided
+        if "-vvv" in sys.argv:
+            logging.basicConfig(
+                level=logging.DEBUG, format="[TEST] %(levelname)5s: %(message)s"
+            )
+
+    @classmethod
+    def run_command(
+        self, cmd: str, working_dir: str = None
+    ) -> tuple[int, str, str]:
+        """
+        Run shell command.
+        returns:
+        - exit code
+        - stdout
+        - stderr
+        """
+        logging.debug("Running: %s", cmd)
+        commands = shlex.split(cmd)
+        with subprocess.Popen(
+            commands,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=working_dir,
+        ) as process:
+            stdout, stderr = process.communicate()
+            return (
+                process.returncode,
+                f"stdout: {stdout.decode('utf-8')}",
+                f"stderr: {stderr.decode('utf-8')}",
+            )
+
+
+# Dynamically add a test method for each project
+for dir_name in PROJECT_DIRS:
+
+    def create_test_method(directory_name):
+        def test_runner(self):
+            project_root = os.path.join(ROOT_DIR, directory_name)
+
+            self.assertTrue(
+                os.path.exists(os.path.join(project_root, "init.sh")),
+                f"Missing 'init.sh' in {directory_name}",
+            )
+            project_working_dir = os.path.join(project_root, "test-proj")
+            if not os.path.exists(project_working_dir):
+                ret, _, _ = self.run_command("sh init.sh", project_root)
+            self.assertTrue(os.path.exists(project_working_dir))
+            ret, _, _ = self.run_command(
+                "bazel build :codechecker_test", project_working_dir
+            )
+            self.assertEqual(ret, 0)
+            ret, _, _ = self.run_command(
+                "bazel build :code_checker_test", project_working_dir
+            )
+            self.assertEqual(ret, 0)
+
+        return test_runner
+
+    test_name = f"test_{dir_name}"
+    setattr(DynamicFOSSTest, test_name, create_test_method(dir_name))
+
+if __name__ == "__main__":
+    unittest.main()
