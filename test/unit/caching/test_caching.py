0850ee1559db5ed5030bd9e3822e0aa84e78f48e
F.Tibor <tibi.furesz@gmail.com>
2025-09-30 15:05:12 +0200

    Fix unittest: `caching` (#93)
    Why:
    This unittest, if interrupted by the user while running, leaves its tmp
    directory behind, causing future runs of the test to fail.
    
    What:
    Added a clause to remove the folder during setup if present
    
    Addresses:
    fixes: #96
    
    ---------
    
    Co-authored-by: Kristóf Umann <dkszelethus@gmail.com>

--- test/unit/caching/test_caching.py
+++ test/unit/caching/test_caching.py
@@ -44,6 +44,13 @@ class TestCaching(TestBase):
     def setUp(self):
         """Before every test: clean Bazel cache"""
         super().setUp()
+        # If the test was unexpectedly stopped, we need to
+        # cleanup from the previous run.
+        if os.path.exists("tmp"):
+            try:
+                shutil.rmtree("tmp")
+            except Exception as e:
+                self.fail(f"Failed to clean up the existing tmp directory {e}")
         os.mkdir("tmp")
         shutil.copy("primary.cc", "tmp")
         shutil.copy("secondary.cc", "tmp")
