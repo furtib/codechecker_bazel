8171c07fa78b1f6618cd25016bab28e30e2b4a97
F.Tibor <tibi.furesz@gmail.com>
2026-02-05 11:35:57 +0100

    Apply pylint suggestions to per_file_script.py (#179)
    Why:
    We want pylint to pass in CI
    
    What:
    - Applied pylint suggestions to `per_file_script.py`.
    
    Addresses:
    #153

--- src/per_file_script.py
+++ src/per_file_script.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Codechecker wrapper script for per-file analysis
+"""
 
 import os
 import re
@@ -33,32 +36,17 @@ COMPILE_COMMANDS_JSON: str = "{compile_commands_json}"
 COMPILE_COMMANDS_ABSOLUTE: str = f"{COMPILE_COMMANDS_JSON}.abs"
 CODECHECKER_ARGS: str = "{codechecker_args}"
 CONFIG_FILE: str = "{config_file}"
-
-
-def parse_args():
-    """
-    Parse arguments that may change from file-to-file.
-    """
-    if len(sys.argv) != 5:
-        print("Wrong amount of arguments")
-        sys.exit(1)
-
-    global DATA_DIR
-    global FILE_PATH
-    global LOG_FILE
-    global ANALYZER_PLIST_PATHS
-
-    DATA_DIR = sys.argv[1]
-    FILE_PATH = sys.argv[2]
-    LOG_FILE = sys.argv[3]
-    ANALYZER_PLIST_PATHS = [item.split(",") for item in sys.argv[4].split(";")]
+DATA_DIR = sys.argv[1]
+FILE_PATH = sys.argv[2]
+LOG_FILE = sys.argv[3]
+ANALYZER_PLIST_PATHS = [item.split(",") for item in sys.argv[4].split(";")]
 
 
 def log(msg: str) -> None:
     """
     Append message to the log file
     """
-    with open(LOG_FILE, "a") as log_file:  # type: ignore
+    with open(LOG_FILE, "a", encoding="utf-8") as log_file:  # type: ignore
         log_file.write(msg)
 
 
@@ -67,8 +55,10 @@ def _create_compile_commands_json_with_absolute_paths():
     Modifies the paths in compile_commands.json to contain the absolute path
     of the files.
     """
-    with open(COMPILE_COMMANDS_JSON, "r") as original_file, open(
-        COMPILE_COMMANDS_ABSOLUTE, "w"
+    with open(
+        COMPILE_COMMANDS_JSON, "r", encoding="utf-8"
+    ) as original_file, open(
+        COMPILE_COMMANDS_ABSOLUTE, "w", encoding="utf-8"
     ) as new_file:
         content = original_file.read()
         # Replace "directory":"." with the absolute path
@@ -102,12 +92,12 @@ def _run_codechecker() -> None:
         env=os.environ,
         capture_output=True,
         text=True,
+        check=False,
     )
     log(result.stdout)
 
-
     try:
-        with open(LOG_FILE, "a") as log_file:  # type: ignore
+        with open(LOG_FILE, "a", encoding="utf-8") as log_file:  # type: ignore
             subprocess.run(
                 codechecker_cmd,
                 env=os.environ,
@@ -128,7 +118,7 @@ def _display_error(ret_code: int) -> None:
     # Log and exit on error
     print("===-----------------------------------------------------===")
     print(f"[ERROR]: CodeChecker returned with {ret_code}!")
-    with open(LOG_FILE, "r") as log_file:  # type: ignore
+    with open(LOG_FILE, "r", encoding="utf-8") as log_file:  # type: ignore
         print(log_file.read())
     sys.exit(1)
 
@@ -144,13 +134,22 @@ def _move_plist_files():
             if re.search(
                 rf"_{analyzer_info[0]}_.*\.plist$", file
             ) and os.path.isfile(
-                os.path.join(DATA_DIR, file)
-            ):  # type: ignore
-                shutil.move(os.path.join(DATA_DIR, file), analyzer_info[1])  # type: ignore
+                os.path.join(DATA_DIR, file)  # type: ignore
+
+            ):
+                shutil.move(
+                    os.path.join(DATA_DIR, file),   # type: ignore
+                    analyzer_info[1],
+                    )
 
 
 def main():
-    parse_args()
+    """
+    Main function of CodeChecker wrapper
+    """
+    if len(sys.argv) != 5:
+        print("Wrong amount of arguments")
+        sys.exit(1)
     _create_compile_commands_json_with_absolute_paths()
     _run_codechecker()
     _move_plist_files()
@@ -162,5 +161,6 @@ if __name__ == "__main__":
 
 # I have conserved this comment from the original bash script
 # The sed commands are commented out, so we won't implement them
-# # sed -i -e "s|<string>.*execroot/bazel_codechecker/|<string>|g" $CLANG_TIDY_PLIST
-# # sed -i -e "s|<string>.*execroot/bazel_codechecker/|<string>|g" $CLANGSA_PLIST
+# sed -i -e "s|<string>.*execroot/bazel_codechecker/|<string>|g" \
+# $CLANG_TIDY_PLIST
+# sed -i -e "s|<string>.*execroot/bazel_codechecker/|<string>|g" $CLANGSA_PLIST
